a712c50c7791f9abdd1bd843b225076706f10317
NishantKumbhare <nishant.kumbhare@globallogic.com>
2017-09-07 20:25:09 +0530

    Added WeatherConstant and WeatherAPI for HTTP request.

---
+++ WeatherApp/WeatherApp/Webservice/WeatherAPI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using WeatherApp.Constant;
+
+namespace WeatherApp.Webservice
+{
+    public class WeatherAPI : ContentPage
+    {
+        public WeatherAPI()
+        {
+            
+        }
+
+        public async Task GetWeatherData()
+        {
+            var client = new System.Net.Http.HttpClient();
+            client.BaseAddress = new Uri(WeatherConstant.URLBASE);
+            var response = await client.GetAsync(WeatherConstant.APIKEY+WeatherConstant.URLWEATHER);
+            var weatherJson = response.Content.ReadAsStringAsync().Result;
+            System.Diagnostics.Debug.WriteLine("response::- " + weatherJson);
+        }
+    }
+}
+

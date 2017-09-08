73c7f06dea322ca37a03e413af29f4c641b342cb
NishantKumbhare <nishant.kumbhare@globallogic.com>
2017-09-08 18:41:15 +0530

    Model and Network API and view model added.

---
+++ WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WeatherApp.Model;
+using Xamarin.Forms;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM
+    {
+        public string Fahrenheit {
+            get => Fahrenheit;
+            set => Fahrenheit = new WeatherModel().current_observation.temp_c;
+        }
+
+        public string Celsius
+        {
+            get => Celsius;
+            set => Celsius = new WeatherModel().current_observation.temp_c;
+        }
+
+        public string Time
+        {
+            get => Time;
+            set => Time = new WeatherModel().current_observation.observation_time_rfc822;
+        }
+
+    }
+}
+
--- WeatherApp/WeatherApp/Webservice/WeatherAPI.cs
+++ WeatherApp/WeatherApp/Webservice/WeatherAPI.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using WeatherApp.Constant;
+using WeatherApp.Model;
 
 namespace WeatherApp.Webservice
 {
@@ -15,13 +16,22 @@ namespace WeatherApp.Webservice
             
         }
 
-        public async Task GetWeatherData()
+        public async Task<WeatherModel> GetWeatherData()
         {
-            var client = new System.Net.Http.HttpClient();
-            client.BaseAddress = new Uri(WeatherConstant.URLBASE);
-            var response = await client.GetAsync(WeatherConstant.APIKEY+WeatherConstant.URLWEATHER);
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(Weather.URLBASE);
+            var response = await client.GetAsync(Weather.APIKEY+Weather.URLWEATHER);
             var weatherJson = response.Content.ReadAsStringAsync().Result;
             System.Diagnostics.Debug.WriteLine("response::- " + weatherJson);
+
+            WeatherModel weatherModel = new WeatherModel();
+            if (weatherJson != null){
+                weatherModel  = JsonConvert.DeserializeObject<WeatherModel>(weatherJson);
+                System.Diagnostics.Debug.WriteLine("Fahrenheit:- " + weatherModel.current_observation.temp_f);
+                System.Diagnostics.Debug.WriteLine("Celsius:- " + weatherModel.current_observation.temp_c);
+                System.Diagnostics.Debug.WriteLine("Request Time:- " + weatherModel.current_observation.observation_time_rfc822);
+            }
+            return weatherModel;
         }
     }
 }

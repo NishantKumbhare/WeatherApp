fb82e91b4a7dfde7bf48fa9656fd59b2340377cd
NishantKumbhare <nishant.kumbhare@globallogic.com>
2017-09-11 10:50:24 +0530

    Constant fie changes.

---
+++ WeatherApp/WeatherApp/Constant/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace WeatherApp.Constant
+{
+    public class Weather
+    {
+        public static string URLBASE = "http://api.wunderground.com/api/";
+        public static string APIKEY = "29f6eba07599dc89";
+        public static string URLWEATHER = "/conditions/q/CA/San_Francisco.json";
+
+    }
+}
+
